d67064ed7d74263bad32d00df91a52ce566d86f8
Mohamad <mohamad@Mohamads-Mac.local>
2014-07-31 22:40:32 +0300

    Make the dimensions relative to UIButton frame

--- Shutup Button/ShutupButton.swift
+++ Shutup Button/ShutupButton.swift
@@ -12,43 +12,86 @@ import CoreGraphics
 
 class ShutupButton: UIButton {
     
+    var mute:Bool = false
+    var speakerLayer: CAShapeLayer! = CAShapeLayer()
+    var beat1Layer: CAShapeLayer! = CAShapeLayer()
+    var beat2Layer: CAShapeLayer! = CAShapeLayer()
+    let enabledStrokeStart:CGFloat = 0.0
+    let enabledStrokeEnd:CGFloat = 1.0
+    
+    let muteStrokeStart:CGFloat = 0.1
+    let muteStrokeEnd: CGFloat = 0.9
     
     
-    let beat1: CGPath = {
-        let path = CGPathCreateMutable()
-        CGPathMoveToPoint(path,nil,52.5,21.5)
-        CGPathAddQuadCurveToPoint(path,nil,55,30,52.5,38.5)
-        return path
-    }()
-    
-    let beat2: CGPath = {
-        let path = CGPathCreateMutable()
-        CGPathMoveToPoint(path,nil,67.5,15)
-        CGPathAddQuadCurveToPoint(path,nil,75,30,67.5,45)
-        return path
-        }()
-    
-    
-    let Speaker: CGPath = {
-        let path = CGPathCreateMutable()
-        CGPathMoveToPoint(path,nil,42.5,55)
-        CGPathAddLineToPoint(path,nil,42.5,5)
-        CGPathAddLineToPoint(path,nil,25,17.5)
-        CGPathAddLineToPoint(path,nil,5,17.5)
-        CGPathAddLineToPoint(path,nil,5,42.5)
-        CGPathAddLineToPoint(path,nil,25,42.5)
-        CGPathAddLineToPoint(path,nil,42.5,55)
-        return path
-        }()
     
     
     
     init(frame: CGRect) {
-        super.init(frame: CGRectMake(0,0,0,0))
+        super.init(frame: frame)
+        
+        let h  = frame.size.height
+        let w = frame.size.width
+        
+        let beat1Stroke: CGPath = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path,nil,0,0)
+            CGPathAddQuadCurveToPoint(path,nil,w*0.07,(h*0.34/2),0,h*0.34)
+            return path
+            }()
+        
+        let beat2Stroke: CGPath = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path,nil,0,0)
+            CGPathAddQuadCurveToPoint(path,nil,w*0.14,(h*0.6/2),0,h*0.6)
+            return path
+            }()
+        
+        
+        let speakerStroke: CGPath = {
+            let path = CGPathCreateMutable()
+            CGPathMoveToPoint(path,nil,w/2,h)
+            CGPathAddLineToPoint(path,nil,w/2,0)
+            CGPathAddLineToPoint(path,nil,(w/2)*40/75,h*0.25)
+            CGPathAddLineToPoint(path,nil,0,h*0.25)
+            CGPathAddLineToPoint(path,nil,0,h*0.75)
+            CGPathAddLineToPoint(path,nil,(w/2)*40/75,h*0.75)
+            CGPathAddLineToPoint(path,nil,w/2,h)
+            return path
+            }()
+
+        
+        
+        self.speakerLayer.path = speakerStroke
+        self.beat1Layer.path = beat1Stroke
+        self.beat2Layer.path = beat2Stroke
+        
+        for layer in [speakerLayer,beat1Layer, beat2Layer] {
+            layer.fillColor = nil
+            layer.strokeColor = UIColor.whiteColor().CGColor
+            layer.lineWidth = 4
+            layer.miterLimit = 2
+            layer.lineCap = kCALineCapRound
+            layer.masksToBounds = true
+            
+            let strokingPath = CGPathCreateCopyByStrokingPath(layer.path, nil, 4, kCGLineCapRound, kCGLineJoinMiter, 4)
+            
+            layer.bounds = CGPathGetPathBoundingBox(strokingPath)
+            
+            
+            layer.actions = [
+                "strokeStart": NSNull(),
+                "strokeEnd": NSNull(),
+                "transform": NSNull()
+            ]
+            
+            self.layer.addSublayer(layer)
+        }
+        
+        self.beat1Layer.position = CGPointMake(w*0.55,0)
+        self.beat2Layer.position = CGPointMake(w*0.75,0)
     }
     
     
-    var mute:Bool = false
     
     
 }
